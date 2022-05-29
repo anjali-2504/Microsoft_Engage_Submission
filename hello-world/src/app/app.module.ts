411771ad40341ab724c9f4c26973486c6a10c5ba
anjali-2504 <anjaliraj25022002@gmail.com>
2022-05-29 11:23:01 +0530

    removed not needed files

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,23 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodosComponent } from './Mycomponenets/todos/todos.component';
-import { TodoItemComponent } from './Mycomponenets/todo-item/todo-item.component';
-import { AddTodosItemComponent } from './Mycomponenets/add-todos-item/add-todos-item.component';
 import { FormsModule } from '@angular/forms';
 import {MatTabsModule} from '@angular/material/tabs';
 import { WebcamComponent } from './webcam/webcam.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FaceRecogComponent } from './face-recog/face-recog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent,
-    TodoItemComponent,
-    AddTodosItemComponent,
     WebcamComponent,
-    FaceRecogComponent
   ],
   imports: [
     BrowserModule,
@@ -27,8 +19,6 @@ import { FaceRecogComponent } from './face-recog/face-recog.component';
     FormsModule,
     BrowserAnimationsModule,
     MatTabsModule,
-    //MatSidenavModule
-    //HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
